1ea34865f3ca132d1d0db3a5a8d49dc2fbdd99f4
M Nu'man Arif H <numanarif87@gmail.com>
2023-02-07 14:49:50 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PowerIcon } from "@heroicons/react/24/outline";
+
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
@@ -9,19 +11,25 @@ const Navbar = () => {
   };
   return (
     <nav>
-      <div>
-        <ul className={styles.navbar}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+      <div className="nav-wrapper">
+        <div className="navbar-title">
+          <ul className={styles.navbar}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
 
-          <li>
-            <Link to="/done">Done</Link>
-          </li>
-          <li>
-            <button onClick={logout}>Logout</button>
-          </li>
-        </ul>
+            <li>
+              <Link to="/done">Done</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-logout">
+          <ul>
+            <button onClick={logout}>
+              <PowerIcon width={24} height={24} />
+            </button>
+          </ul>
+        </div>
       </div>
     </nav>
   );
